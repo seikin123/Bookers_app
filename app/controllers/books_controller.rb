140550855b1e767e13ee99fab840239def4a055d
ヨシダタイキ <abc521fjr7@gmail.com>
2021-04-06 16:33:36 +0900

    [add]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
@@ -13,12 +14,25 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to books_path
+    redirect_to books_path(book.id)
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
+  end
+
+    def destroy
+      book=Book.find(params[:id])
+      book.destroy
+      redirect_to books_path
+    end
+    
   private
   def book_params
     params.require(:book).permit(:title, :body)
