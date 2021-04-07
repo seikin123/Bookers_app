dbdbc9ce6c9ff0dc66fc8f0c1a30c36b4d9cc74d
ヨシダタイキ <abc521fjr7@gmail.com>
2021-04-07 11:11:55 +0900

    [修正]createメソッド

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,11 @@
 class BooksController < ApplicationController
+
   def index
     @books = Book.all
+    @book = Book.new
+  end
+
+  def top
   end
 
   def show
@@ -13,9 +18,12 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+  if book.save
+    redirect_to book_path(book.id), notice: 'Book was successfully created!' 
+  else
+    render "index"
   end
+end
 
   def edit
     @book = Book.find(params[:id])
@@ -23,16 +31,19 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+  if book.update(book_params)
+    redirect_to book_path(book), notice: 'Book was successfully updated!'
+  else
+    render action: :edit
+  end
   end
 
     def destroy
       book=Book.find(params[:id])
       book.destroy
-      redirect_to books_path
+      redirect_to books_path, notice: 'Book was successfully destroy'
     end
-    
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
